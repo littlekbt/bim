345abdb48bd5b525e9f102e8dc16d041a24b54fd
littlekbt <kr.kubota.11@gmail.com>
2017-11-14 11:12:37 +0900

    rubocop

--- lib/bim/action/fw.rb
+++ lib/bim/action/fw.rb
@@ -7,6 +7,30 @@ module Bim
       FW_PATH = '/mgmt/tm/security/firewall/policy'.freeze
       FW_URI = URI.join(Bim::BASE_URL, Bim::Action::Fw::FW_PATH)
 
+      DETAIL_CONF = {
+        name: 'firewall_policy',
+        items: false,
+        attrs: %w[name fullPath],
+        under_key: 'rulesReference',
+        under_layer: {
+          name: 'rules_reference',
+          items: true,
+          attrs: %w[name description ipProtocol ruleList],
+          under_key: 'ruleListReference',
+          under_layer: {
+            name: 'rule_list_reference',
+            items: false,
+            attrs: %w[name fullPath],
+            under_key: 'rulesReference',
+            under_layer: {
+              name: 'rules_reference',
+              items: true,
+              attrs: %w[name description destination source]
+            }
+          }
+        }
+      }.freeze
+
       class << self
         def ls
           map(FW_URI) do |item|
@@ -16,34 +40,9 @@ module Bim
 
         def detail(name)
           start_uri = URI(sub_localhost(specify_link_by_name(FW_URI, name)))
-          conf = {
-            name: 'firewall_policy',
-            items: false,
-            attrs: ['name', 'fullPath'], 
-            under_key: 'rulesReference', 
-            under_layer: {
-              name: 'rules_reference',
-              items: true,
-              attrs: ['name', 'description', 'ipProtocol', 'ruleList'],
-              under_key: 'ruleListReference',
-              under_layer: {
-                name: 'rule_list_reference',
-                items: false, 
-                attrs: ['name', 'fullPath'], 
-                under_key: 'rulesReference',
-                under_layer: {
-                  name: 'rules_reference',
-                  items: true, 
-                  attrs: ['name', 'description', 'destination', 'source']
-                }
-              }
-            }
-          }
- 
-          detail_depth(start_uri, conf).to_json
+          detail_depth(start_uri, DETAIL_CONF).to_json
         end
 
-        # itemsの時は配列、それ以外はhashを返して上の階層が下の階層のキーで保管すればOK
         def detail_depth(uri, conf)
           datas = JSON.parse(get_body(uri))
           if conf[:items]
@@ -56,7 +55,7 @@ module Bim
         end
 
         def detail_proc
-          Proc.new do |data, conf, map=false|
+          proc do |data, conf, map = false|
             d = {}
             conf[:attrs].each { |attr| d[attr] = data[attr] }
 
